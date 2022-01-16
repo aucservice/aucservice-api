63382167ddc49c0c2e80a7f5edada049830cc8d3
Herman <herman@spiralarms.org>
2022-01-16 12:33:57 +0300

    require login for delete and put

--- api.py
+++ api.py
@@ -1,8 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse, abort
-import datetime
-import os
-import jwt
+import datetime, os, jwt, functools
 
 app = Flask(__name__)
 api = Api(app)
@@ -22,18 +20,40 @@ users = {
 parser = reqparse.RequestParser()
 parser.add_argument('price')
 
+def login_required(method):
+	@functools.wraps(method)
+	def wrapper(*args, **kwargs):
+		header = request.headers.get('Authorization')
+		if header is None:
+			abort(400, message='Login is required')
+		_, token = header.split()
+		try:
+			decoded = jwt.decode(token, KEY, algorithms='HS256')
+		except jwt.DecodeError:
+			abort(400, message='Token is not valid.')
+		except jwt.ExpiredSignatureError:
+			abort(400, message='Token is expired.')
+		username = decoded['username']
+		if username not in users:
+			abort(400, message=f'User {username} is not found.')
+		user = users[username]
+		return method(*args, **kwargs)
+	return wrapper
+
 class Item(Resource):
 	def get(self, item_id):
 		if item_id not in items:
 			abort(404, message=f"Item {item_id} does not exist")
 		return items[item_id]
 
+	@login_required
 	def delete(self, item_id):
 		if item_id not in items:
 			abort(404, message=f"Item {item_id} does not exist")
 		del items[item_id]
 		return '', 204
 
+	@login_required
 	def put(self, item_id):
 		args = parser.parse_args()
 		item = {'price': args['price']}
@@ -59,8 +79,8 @@ class Login(Resource):
 		return {'username': username, 'token': token}
 
 class HelloWorld(Resource):
-    def get(self):
-        return {'name': 'aucservice'}
+	def get(self):
+		return {'name': 'aucservice'}
 
 api.add_resource(HelloWorld, '/')
 api.add_resource(Items, '/items')
@@ -68,4 +88,4 @@ api.add_resource(Item, '/items/<item_id>')
 api.add_resource(Login, '/login')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
